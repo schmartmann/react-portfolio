c56a5866a721e99cae0a651fe909c70116181da7
Stefan Hartmann <stefanhartmann@gmail.com>
2017-03-13 11:44:53 -0400

    swap hashHistory for browserHistory in react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 import App from './App';
 import Writing from './Writing';
@@ -14,7 +14,7 @@ import './index.css';
 
 
 ReactDOM.render((
-  <Router history={ hashHistory } >
+  <Router history={ browserHistory } >
     <Route path="/" component={ App }>
       <IndexRoute component={ Splash }/>
       <Route path="/writing" component={ Writing } />
