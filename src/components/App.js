0b8d829500e4950c16e0965bbc9e10c1fcc4fa8d
schmartmann <stefanhartmann@gmail.com>
2018-08-14 19:49:19 -0400

    conditional background styling on route change

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../stylesheets/App.css';
 
+var colorScheme = {
+  '/writing': {
+    background: '#ffde8d',
+    color: '#064273'
+  },
+  '/code': {
+    background: '#E4939E',
+    color: '#40FFE6'
+  },
+  '/contact': {
+    background: 'black',
+    color: 'white'
+  },
+  '/about': {
+    color: '#CFE1B2',
+    background: '#5771B2'
+  },
+  '/home': {
+    background: '#349884'
+  },
+  '/': {
+    background: '#349884'
+  }
+};
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={ colorScheme[ this.props.location.pathname ] }>
         { this.props.children }
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import Writing from './components/Writing';
@@ -9,27 +9,22 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Splash from './components/Splash';
-import assignBackground from './components/Background';
+// import assignBackground from './components/Background';
 import './index.css';
 
 ReactDOM.render(
   (
     <Router history={ browserHistory } >
       <Route path="/" component={ App }>
-        <IndexRoute component={ Home }
-          onEnter={ assignBackground( '/home') } />
-        <Route path="/writing"
-          component={ Writing }
-          onEnter={ assignBackground( '/writing') } />
+        <IndexRoute component={ Home } />
+          <Route path="/writing"
+          component={ Writing } />
         <Route path="/code"
-          component={ Code }
-          onEnter={ assignBackground( '/code') } />
+          component={ Code } />
         <Route path="/about"
-          component={ About }
-          onEnter={ assignBackground( '/about') }/>
+          component={ About } />
         <Route path="/contact"
-          component={ Contact }
-          onEnter={ assignBackground( '/contact') }/>
+          component={ Contact } />
       </Route>
     </Router>
   ),
