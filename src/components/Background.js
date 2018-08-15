6979592a8032c99682d44b68a920b9c52833fa3b
schmartmann <stefanhartmann@gmail.com>
2018-08-14 20:26:28 -0400

    comparmentalize background colors & load on componentWillMount

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,26 +1,24 @@
-export default function assignBackground( pathName ) {
-  var colors = {
-    '/writing': {
-      background: '#ffde8d',
-      color: '#064273'
-    },
-    '/code': {
-      background: '#E4939E',
-      color: '#40FFE6'
-    },
-    '/contact': {
-      background: 'black',
-      color: 'white'
-    },
-    '/about': {
-      color: '#CFE1B2',
-      background: '#5771B2'
-    },
-    '/home': {
-      background: '#349884'
-    }
+export const colorSchemes = {
+  '/writing': {
+    background: '#ffde8d',
+    color: '#064273'
+  },
+  '/code': {
+    background: '#E4939E',
+    color: '#40FFE6'
+  },
+  '/contact': {
+    background: 'black',
+    color: 'white'
+  },
+  '/about': {
+    color: '#CFE1B2',
+    background: '#5771B2'
+  },
+  '/home': {
+    background: '#349884'
+  },
+  '/': {
+    background: '#349884'
   }
-
-  document.body.style.background = colors[ pathName ].background;
-  document.body.style.background = colors[ pathName ].background;
-};
+}
--- src/components/Code.js
+++ src/components/Code.js
@@ -3,40 +3,8 @@ import Back from './Back';
 import Portrait from './Portrait';
 import ImageLoader from 'react-imageloader';
 import '../stylesheets/Code.css';
-
-
-const projects = [
-  {
-    title: "Markup Masker",
-    description: "Ruby Gem that takes in a string polluted with HTML or JavaScript, and uses regexs to break separate out unwanted markup.",
-    href: "https://github.com/schmartmann/markup_mask",
-    src: "https://media.giphy.com/media/8UdbDVaUtYNhK/giphy.gif"
-  },
-  {
-    title: 'Sinatra Turntable',
-    description: 'Bash script that bootstraps a Sinatra app with an ActiveRecord database.',
-    href: 'https://github.com/schmartmann/sinatra_turntable',
-    src: "https://camo.githubusercontent.com/e3874a36a46fb5cefeb9fee7c12739ef98ce74e3/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f687278586e7a566f4c7237346b2f67697068792e676966"
-  },
-  {
-    title: 'Now You Know PR',
-    description: `React + Redux / Rails app. Search music publications and bloggers and organize them into campaigns for DIY music publicity.`,
-    href: 'http://www.nowyouknowpr.com',
-    src: 'img/nyk.png'
-  },
-  {
-    title: 'peTinder',
-    description: 'ReactJS + Redux & Rails app that geolocates users and finds adoptable pets via the Petfinder API.',
-    href: 'http://www.pet-tinder.com/',
-    src: 'img/petinder.png'
-  },
-  {
-    title: 'U-Vote.org',
-    description: 'Sinatra app that drives youth voter registration by gamifying voter registration among universities.',
-    href: 'http://www.u-vote.org',
-    src: 'img/uvote.png'
-  }
-]
+import * as projects from './projects/code/Projects';
+import * as styles from './Background';
 
 class Project extends Component {
   constructor(props) {
@@ -82,13 +50,15 @@ class Project extends Component {
 }
 export default class Code extends Component {
   renderProjects() {
-    return projects.map( (project, i) => {
+    return projects.projects.map( (project, i) => {
       return <Project data={project} key={ i } />
     })
   }
-  componentDidMount() {
-    var body = document.querySelector('body');
-    body.className = 'codeRoute'
+  componentWillMount() {
+    var colorSchemes = styles.colorSchemes;
+    var path = this.props.location.pathname
+    document.body.style.background = colorSchemes[ path ].background;
+    document.body.style.color = colorSchemes[ path ].color;
   }
   render(){
     return(
